01535c67923963cb48ca519299602635e544dbaa
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2019-06-18 01:17:45 +0200

    fix size train_set

--- data_mining_proj.py
+++ data_mining_proj.py
@@ -8,17 +8,23 @@ reader.Update()
 acq = reader.GetOutput() # acq is the btk aquisition object
 
 def get_train_set():
-    train_set = np.array([])
+    captor = 2
+    train_set = np.array([np.zeros(captor+1)])
     for i in range(100):
         try:
             event_frame = acq.GetEvent(i).GetFrame()
             tmp_set = np.array([acq.GetEvent(i).GetLabel(),
-                                    acq.GetPoint('LHEE').GetValues()[event_frame,2],
-                                    acq.GetPoint('RHEE').GetValues()[event_frame,2]])
-            train_set = np.concatenate((train_set,tmp_set), axis=0)
+                                acq.GetPoint('LHEE').GetValues()[event_frame,2],
+                                acq.GetPoint('RHEE').GetValues()[event_frame,2]])
+            tmp_set = np.array([tmp_set])
+            train_set = np.concatenate((train_set, tmp_set), axis=0)
         except Exception as e:
+            train_set = np.delete(train_set, 0, 0)
+            print(train_set.shape)
             return train_set
+
 print(get_train_set())
+
 
 # for i in range(20):
 #     data_FrameRef = np.array([  acq.GetPoint('LHEE').GetValues()[i,2],
