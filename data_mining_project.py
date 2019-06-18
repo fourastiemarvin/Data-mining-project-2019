c450b6eecb83a042f69c77d49226ecaea754b211
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2019-06-18 23:56:12 +0200

    refactored code & try multiple alg

---
+++ data_mining_project.py
@@ -0,0 +1,116 @@
+import sys
+import btk
+import glob
+import random
+import numpy as np
+import pandas as pd
+from sklearn import tree
+from sklearn import linear_model
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+
+
+def get_data(nb_captor):
+    captor = nb_captor
+    list_of_files_CP = glob.glob('./Sofamehack2019/Sub_DB_Checked/CP/*.c3d')
+    #list_of_files_CP = ['./Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d']
+    data_set = np.array([np.zeros(captor+1)])
+
+    for file_name_CP in list_of_files_CP:
+        reader = btk.btkAcquisitionFileReader()
+        reader.SetFilename(file_name_CP)
+        reader.Update()
+        acq = reader.GetOutput()
+        for i in range(100):
+            try:
+                event_frame = acq.GetEvent(i).GetFrame()
+
+                tmp_set = np.array([acq.GetEvent(i).GetLabel(),
+                                    acq.GetPoint('LTOE').GetValues()[event_frame,2],acq.GetPoint('RTOE').GetValues()[event_frame,2],
+                                    acq.GetPoint('LANK').GetValues()[event_frame,2],acq.GetPoint('RANK').GetValues()[event_frame,2],
+                                    acq.GetPoint('LHEE').GetValues()[event_frame,2],acq.GetPoint('RHEE').GetValues()[event_frame,2]])
+                tmp_set = np.array([tmp_set])
+                data_set = np.concatenate((data_set, tmp_set), axis=0)
+                if (acq.GetEvent(i).GetLabel() == 'Foot_Off_GS' or acq.GetEvent(i).GetLabel() == 'Foot_Strike_GS'):
+                    range_no_event = range(event_frame-9,event_frame-1) + range(event_frame+2,event_frame+10)
+                    #print(range_no_event)
+                    for j in range_no_event:
+                        no_event_set = np.array(['No_Event',
+                                                acq.GetPoint('LTOE').GetValues()[j,2],acq.GetPoint('RTOE').GetValues()[j,2],
+                                                acq.GetPoint('LANK').GetValues()[j,2],acq.GetPoint('RANK').GetValues()[j,2],
+                                                acq.GetPoint('LHEE').GetValues()[j,2],acq.GetPoint('RHEE').GetValues()[j,2]])
+                        no_event_set = np.array([no_event_set])
+                        data_set = np.concatenate((data_set, no_event_set), axis=0)
+            except Exception as e:
+                data_set = np.delete(data_set, 0, 0)
+                break
+    y = data_set[:,0]
+    X = data_set[:,1:captor+1].astype(np.float)
+    return (X,y)
+
+def get_prediction(nb_captor,X,y,algo):
+    captor = nb_captor
+    #list_of_files_CP = glob.glob('./Sofamehack2019/Sub_DB_Checked/CP/*.c3d')
+    list_of_files_CP = ['./Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d']
+
+    if (algo == 'DT'):
+        clf = tree.DecisionTreeClassifier()
+    elif (algo == 'NB'):
+        clf = GaussianNB()
+    elif (algo == 'KNN'):
+        clf = NearestCentroid()
+    clf.fit(X, y)
+
+    for file_name_CP in list_of_files_CP:
+        print("\n")
+        X_test = np.array([np.zeros(captor)])
+        reader = btk.btkAcquisitionFileReader()
+        reader.SetFilename(file_name_CP)
+        reader.Update()
+        acq = reader.GetOutput()
+        #print(file_name_CP)
+        for frame in range(1,500,1):
+            try:
+                tmp_X_test = np.array([acq.GetPoint('LTOE').GetValues()[frame,2],acq.GetPoint('RTOE').GetValues()[frame,2],
+                                        acq.GetPoint('LANK').GetValues()[frame,2],acq.GetPoint('RANK').GetValues()[frame,2],
+                                        acq.GetPoint('LHEE').GetValues()[frame,2],acq.GetPoint('RHEE').GetValues()[frame,2]])
+                tmp_X_test = np.array([tmp_X_test])
+                X_test = np.concatenate((X_test, tmp_X_test), axis=0)
+            except Exception as e:
+                X_test = np.delete(X_test, 0, 0)
+                break
+        if (algo == 'DT'):
+            pred_decisionTree = decision_tree(X,y,X_test,clf)
+        elif (algo == 'NB'):
+            pred_gaussianNB = gaussian_naive_bayes(X,y,X_test,clf)
+        elif (algo == 'KNN'):
+            pred_KNN = K_NN(X,y,X_test,clf)
+    return
+
+def decision_tree(X,y,X_test,clf):
+    predictions_DT = clf.predict(X_test)
+    df = pd.DataFrame(data = predictions_DT, columns = ['predictions_DT'])
+    print(df.loc[df['predictions_DT'] != 'No_Event'])
+    #print(df)
+    return predictions_DT
+
+def gaussian_naive_bayes(X,y,X_test,clf):
+    predictions_NB = clf.predict(X_test)
+    df = pd.DataFrame(data = predictions_NB, columns = ['predictions_NB'])
+    print(df.loc[df['predictions_NB'] != 'No_Event'])
+    #print(df)
+    return predictions_NB
+
+def K_NN(X,y,X_test,clf):
+    predictions_KNN = clf.predict(X_test)
+    df = pd.DataFrame(data = predictions_KNN, columns = ['predictions_KNN'])
+    print(df.loc[df['predictions_KNN'] != 'No_Event'])
+    #print(df)
+    return predictions_KNN
+
+def main():
+    nb_capt = 6
+    [X,y] = get_data(nb_capt)
+    get_prediction(nb_capt,X,y,'DT')
+
+main()
