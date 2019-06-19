44c5784922d76218d46a3740532a75b7fbd0ce43
sardinhapatrick <Patrick.Sardinha@etu.unige.ch>
2019-06-19 12:01:00 +0200

    update code & add alg & test

--- data_mining_project.py
+++ data_mining_project.py
@@ -8,7 +8,8 @@ from sklearn import tree
 from sklearn import linear_model
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors.nearest_centroid import NearestCentroid
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 
 def get_data(nb_captor):
     captor = nb_captor
@@ -27,18 +28,18 @@ def get_data(nb_captor):
 
                 tmp_set = np.array([acq.GetEvent(i).GetLabel(),
                                     acq.GetPoint('LTOE').GetValues()[event_frame,2],acq.GetPoint('RTOE').GetValues()[event_frame,2],
-                                    acq.GetPoint('LANK').GetValues()[event_frame,2],acq.GetPoint('RANK').GetValues()[event_frame,2],
-                                    acq.GetPoint('LHEE').GetValues()[event_frame,2],acq.GetPoint('RHEE').GetValues()[event_frame,2]])
+                                    acq.GetPoint('LHEE').GetValues()[event_frame,2],acq.GetPoint('RHEE').GetValues()[event_frame,2],
+                                    acq.GetPoint('LANK').GetValues()[event_frame,2],acq.GetPoint('RANK').GetValues()[event_frame,2]])
                 tmp_set = np.array([tmp_set])
                 data_set = np.concatenate((data_set, tmp_set), axis=0)
                 if (acq.GetEvent(i).GetLabel() == 'Foot_Off_GS' or acq.GetEvent(i).GetLabel() == 'Foot_Strike_GS'):
-                    range_no_event = range(event_frame-9,event_frame-1) + range(event_frame+2,event_frame+10)
+                    range_no_event = range(event_frame-4,event_frame-1) + range(event_frame+2,event_frame+5)
                     #print(range_no_event)
                     for j in range_no_event:
                         no_event_set = np.array(['No_Event',
                                                 acq.GetPoint('LTOE').GetValues()[j,2],acq.GetPoint('RTOE').GetValues()[j,2],
-                                                acq.GetPoint('LANK').GetValues()[j,2],acq.GetPoint('RANK').GetValues()[j,2],
-                                                acq.GetPoint('LHEE').GetValues()[j,2],acq.GetPoint('RHEE').GetValues()[j,2]])
+                                                acq.GetPoint('LHEE').GetValues()[j,2],acq.GetPoint('RHEE').GetValues()[j,2],
+                                                acq.GetPoint('LANK').GetValues()[j,2],acq.GetPoint('RANK').GetValues()[j,2]])
                         no_event_set = np.array([no_event_set])
                         data_set = np.concatenate((data_set, no_event_set), axis=0)
             except Exception as e:
@@ -50,15 +51,19 @@ def get_data(nb_captor):
 
 def get_prediction(nb_captor,X,y,algo):
     captor = nb_captor
-    #list_of_files_CP = glob.glob('./Sofamehack2019/Sub_DB_Checked/CP/*.c3d')
-    list_of_files_CP = ['./Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d']
+    list_of_files_CP = glob.glob('./Sofamehack2019/Sub_DB_Checked/CP/*.c3d')
+    #list_of_files_CP = ['./Sofamehack2019/Sub_DB_Checked/CP/CP_GMFCS1_01916_20130128_18.c3d']
 
     if (algo == 'DT'):
         clf = tree.DecisionTreeClassifier()
     elif (algo == 'NB'):
         clf = GaussianNB()
-    elif (algo == 'KNN'):
+    elif (algo == 'KNN_Centroid'):
         clf = NearestCentroid()
+    elif (algo == 'KNN'):
+        clf = KNeighborsClassifier(n_neighbors=3)
+    elif (algo == 'MLP'):
+        clf = MLPClassifier(hidden_layer_sizes=(3, 3))
     clf.fit(X, y)
 
     for file_name_CP in list_of_files_CP:
@@ -68,12 +73,12 @@ def get_prediction(nb_captor,X,y,algo):
         reader.SetFilename(file_name_CP)
         reader.Update()
         acq = reader.GetOutput()
-        #print(file_name_CP)
+        print(file_name_CP)
         for frame in range(1,500,1):
             try:
                 tmp_X_test = np.array([acq.GetPoint('LTOE').GetValues()[frame,2],acq.GetPoint('RTOE').GetValues()[frame,2],
-                                        acq.GetPoint('LANK').GetValues()[frame,2],acq.GetPoint('RANK').GetValues()[frame,2],
-                                        acq.GetPoint('LHEE').GetValues()[frame,2],acq.GetPoint('RHEE').GetValues()[frame,2]])
+                                        acq.GetPoint('LHEE').GetValues()[frame,2],acq.GetPoint('RHEE').GetValues()[frame,2],
+                                        acq.GetPoint('LANK').GetValues()[frame,2],acq.GetPoint('RANK').GetValues()[frame,2]])
                 tmp_X_test = np.array([tmp_X_test])
                 X_test = np.concatenate((X_test, tmp_X_test), axis=0)
             except Exception as e:
@@ -83,11 +88,16 @@ def get_prediction(nb_captor,X,y,algo):
             pred_decisionTree = decision_tree(X,y,X_test,clf)
         elif (algo == 'NB'):
             pred_gaussianNB = gaussian_naive_bayes(X,y,X_test,clf)
+        elif (algo == 'KNN_Centroid'):
+            pred_KNN_centroid = K_NN_Centroid(X,y,X_test,clf)
         elif (algo == 'KNN'):
             pred_KNN = K_NN(X,y,X_test,clf)
+        elif (algo == 'MLP'):
+            pred_MLP = MLP(X,y,X_test,clf)
     return
 
 def decision_tree(X,y,X_test,clf):
+    #range_no_event = range(event_frame-9,event_frame-1) + range(event_frame+2,event_frame+10)
     predictions_DT = clf.predict(X_test)
     df = pd.DataFrame(data = predictions_DT, columns = ['predictions_DT'])
     print(df.loc[df['predictions_DT'] != 'No_Event'])
@@ -101,16 +111,32 @@ def gaussian_naive_bayes(X,y,X_test,clf):
     #print(df)
     return predictions_NB
 
+def K_NN_Centroid(X,y,X_test,clf):
+    predictions_KNN_centroid = clf.predict(X_test)
+    df = pd.DataFrame(data = predictions_KNN_centroid, columns = ['predictions_KNN_centroid'])
+    print(df.loc[df['predictions_KNN_centroid'] != 'No_Event'])
+    #print(df.to_string())
+    return predictions_KNN_centroid
+
 def K_NN(X,y,X_test,clf):
+    #range_no_event = range(event_frame-4,event_frame-1) + range(event_frame+2,event_frame+5)
     predictions_KNN = clf.predict(X_test)
     df = pd.DataFrame(data = predictions_KNN, columns = ['predictions_KNN'])
     print(df.loc[df['predictions_KNN'] != 'No_Event'])
-    #print(df)
+    #print(df.to_string())
     return predictions_KNN
+
+def MLP(X,y,X_test,clf):
+    #range_no_event = range(event_frame-4,event_frame-1) + range(event_frame+2,event_frame+5)
+    predictions_MLP = clf.predict(X_test)
+    df = pd.DataFrame(data = predictions_MLP, columns = ['predictions_MLP'])
+    print(df.loc[df['predictions_MLP'] != 'No_Event'])
+    #print(df.to_string())
+    return predictions_MLP
 
 def main():
     nb_capt = 6
     [X,y] = get_data(nb_capt)
-    get_prediction(nb_capt,X,y,'DT')
+    get_prediction(nb_capt,X,y,'MLP')
 
 main()
