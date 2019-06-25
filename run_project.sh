3275ef51b7d79086b0fa3610de12b5f40f9ff68c
fourastiemarvin <marvin.fourastie@etu.unige.ch>
2019-06-25 13:04:43 +0200

    add script to run the code

--- run_project.sh
+++ run_project.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 
-# read $NB_CAPT
 echo "CP, FD or ITW?"
-read $FILE
-export var=$FILE
-cat << EOF > run_project.py
-#!/usr/bin/python
-import subprocess
-from data_mining_project import main
-main(files=subprocess.call(["echo","var"]))
-EOF
-chmod 755 run_project.py
-./run_project.py
-
-# FIXME take off the error in the output
-# main(files=subprocess.call(["echo", "$FILE"])
+read F
+if [ -f out.txt ]; then
+   rm out.txt
+   echo "removed out.txt"
+fi
+python ./data_mining_project.py $F >> out.txt
